dabc71df900f613331bf30f04276722bc9ce3e14
Constantine <olexiy2004@gmail.com>
2025-03-25 12:23:54 +0100

    Basic Structure 2

--- apps/appointments/admin.py
+++ apps/appointments/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Appointment
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'user', 'created_at', 'updated_at')
+    search_fields = ('title', 'description')
+    list_filter = ('date', 'user')
