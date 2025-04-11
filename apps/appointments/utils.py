035308c6b2a1ea500db58212dcc9de2dbb1dad97
Constantine <olexiy2004@gmail.com>
2025-04-11 22:13:32 +0200

    Heap Sort

--- apps/appointments/utils.py
+++ apps/appointments/utils.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-
+import random
 from django.template.defaultfilters import length
 
 
@@ -28,7 +28,7 @@ class QuickSort(SortStrategy):
     """Implements Quick Sort algorithm."""
 
     def sort(self, data, key_func):
-        data = data[:]  # Make a copy to avoid modifying the original list
+        data = data[:]
         if len(data) <= 1:
             return data
         pivot = data[0]
@@ -40,10 +40,36 @@ class QuickSort(SortStrategy):
 
 class HeapSort(SortStrategy):
     """Implements Heap Sort algorithm."""
-
+    #heapsort
     def sort(self, data, key_func):
-        data = data[:]  # Make a copy to avoid modifying the original list
+        data = data[:]
+        total_elements = len(data)
+        for current_index in range(total_elements // 2 - 1, -1, -1):
+            self.heapify(data, total_elements, current_index)
 
+        for end_index in range(total_elements - 1, 0, -1):
+            # Move current largest (root) to the end
+            data[0], data[end_index] = data[end_index], data[0]
+
+            # Heapify the reduced heap
+            self.heapify(data, end_index, 0)
+
+        return data
+
+    def heapify(self, data, heap_size, root_index):
+        largest_index = root_index
+        left_child_index = 2 * root_index + 1
+        right_child_index = 2 * root_index + 2
+
+        if left_child_index < heap_size and data[left_child_index] > data[largest_index]:
+            largest_index = left_child_index
+
+        if right_child_index < heap_size and data[right_child_index] > data[largest_index]:
+            largest_index = right_child_index
+
+        if largest_index != root_index:
+            data[root_index], data[largest_index] = data[largest_index], data[root_index]
+            self.heapify(data, heap_size, largest_index)
         return data
 
 
@@ -52,7 +78,7 @@ class MergeSort(SortStrategy):
     """Implements Merge Sort algorithm."""
 
     def sort(self, data, key_func):
-        data = data[:]  # Make a copy to avoid modifying the original list
+        data = data[:]
         if len(data) <= 1:
             return data
 
@@ -81,8 +107,23 @@ class MergeSort(SortStrategy):
         result.extend(right[r:])
         return result
 
+#Fun algorithms
+class Bogosort(SortStrategy):
+    """Implements Bogosort Sort algorithm."""
 
+    def sort(self, data, key_func):
+        data = data[:]
+        while data != sorted(data):
+            random.shuffle(data)
+        return data
 
+class Gnomesort(SortStrategy):
+    """Implements Bogosort Sort algorithm."""
+
+    def sort(self, data, key_func):
+        data = data[:]
+
+        return data
 
 
 
@@ -102,6 +143,6 @@ class SortContext:
 
 # if __name__ == '__main__':
 #     data = [4, 2, 9, 1, 5, 6]
-#     context = SortContext(MergeSort())
+#     context = SortContext(HeapSort())
 #     sorted_data = context.execute_sort(data, key_func=lambda x: x)
 #     print(sorted_data)
